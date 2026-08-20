9fcdac89c0f241d424ca506cf4b855933d653352
informedeobra4-max <informedeobra4-max@users.noreply.github.com>
2026-08-20 13:59:00 -0300

    Switch to original Supabase instance

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qlsgtgcotsdanodgyenh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsc2d0Z2NvdHNkYW5vZGd5ZW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNDIxNTQsImV4cCI6MjEwMjcxODE1NH0.JR32RxOkqK7rZPiwCLljpZHqKE3JAkGlibr2i1tCdww';
+const supabaseUrl = 'https://ctzaqsuzytydagbuhcmh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0emFxc3V6eXR5ZGFnYnVoY21oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwNzI1MDYsImV4cCI6MjEwMjY0ODUwNn0.qo66yfKQ-9E-IPoM9H9j_4rQym8FG5yTSH-SjRhAYJQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
